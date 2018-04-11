0c1570d430d066e099e0d5d46d589b9acd648e0e
jinyung <wongjinyung@gmail.com>
2018-04-11 23:33:48 +0800

    add calc_power

---
+++ R/calc_power.R
@@ -0,0 +1,8 @@
+# --- calculate the power of r ---
+# r: numeric vector
+# u: numeric vector
+calc_power <- function(r, u) {
+  fit <- lm(log10(u)~log10(r))  # power = slope of log-log model
+  pval <- anova(fit)$P[1]
+  ifelse(pval < 0.05, coef(fit)[2], NA)
+}
