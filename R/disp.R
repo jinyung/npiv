f7d9f5532dcafb34ea2a121ef4e64198157d85d0
jinyung <wongjinyung@gmail.com>
2018-11-14 22:32:39 +0800

    documentation part2

--- R/disp.R
+++ R/disp.R
@@ -7,11 +7,14 @@
 # filter will give undesirable result with NAs), adjust keep_len parameter.
 # Generally it is easier to remove NA than to add later
 
-disp <- function(centroid, body_land, keep_len = TRUE, win_size) {
+disp <- function(body_land, keep_len = TRUE, win_size) {
   # size of moving window
   if (missing(win_size))
     win_size <- 3
   pad <- (win_size - 1) / 2
+  
+  # centroid
+  centroid <- t(apply(body_land, 3, cent))
   
   # calculate distance
   if (keep_len == TRUE)
@@ -27,7 +30,6 @@ disp <- function(centroid, body_land, keep_len = TRUE, win_size) {
   ref_vect <- tail_land - centroid
   if (keep_len == FALSE)
     ref_vect <- ref_vect[(1+pad):(dim(ref_vect)[1] - pad), ]  # to n-2
-  # ref_vect <- (ref_vect[-dim(ref_vect)[1], ] + ref_vect[-1, ]) / 2  # to n-2
   disp_sign <- NULL
   for (i in 1:dim(ref_vect)[1])
     disp_sign[i] <- sign(xy_diff[i, ] %*% ref_vect[i, ])  # sign(0) = 0
--- R/phase_diff.R
+++ R/phase_diff.R
@@ -16,22 +16,3 @@ phase_diff <- function (langle_smooth) {
   return(c(two_pairs_result, all_pairs_result))
 }
 
-# phase_diff2 <- function (langle_smooth) {
-#   limb_comb <- list(ant1_ant2 = c(1, 2), ant1_mand = c(1, 3), 
-#                     ant2_mand = c(2, 3))
-#   # get first derivative
-#   d_langle_smooth <- apply(langle_smooth, 2, dif)
-#   # get the sign of the first derivative to determine +/- slopes
-#   sign_langle_smooth <- apply(d_langle_smooth, 2, sign)
-#   two_pairs_result <- sapply(limb_comb, function(x) {
-#     sign1 <- sign_langle_smooth[, x[1]];
-#     sign2 <- sign_langle_smooth[, x[2]];
-#     inphase <- sum(sign1 == sign2);
-#     sum(inphase) / length(sign1)})
-#   all_pairs_result <- sum(sign_langle_smooth[, 1] == sign_langle_smooth[, 2] & 
-#                             sign_langle_smooth[, 2] == sign_langle_smooth[, 3]) / 
-#     length(sign_langle_smooth[, 1])
-#   return(c(two_pairs_result, all_pairs_result))
-# }
-# 
-
